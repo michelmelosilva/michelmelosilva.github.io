310c06c76cfe86d5738d3e87954761b27b52e093
Michel Silva <michel.silva66@gmail.com>
2023-07-03 09:48:39 -0300

    primeiro commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Replacing urls..."
+
+sed -i 's/http:\/\/localhost:4000/./' _site/*.html
+sed -i 's/<a href=".\/">Home<\/a>/<a href=".\/index.html">Home<\/a>/' _site/*.html
+sed -i 's/\.\.\/assets/\.\/assets/' _site/*.html
+
+echo "Finish."
+
+echo "Waiting 3s to complete changes."
+sleep 3s
+
+
+echo "Starting deploy to http://www.dcc.ufmg.br/~michelms ..."
+
+scp -r _site/* michelms@login.dcc.ufmg.br:~/public_html
+
+echo "Finish."
