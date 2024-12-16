a41121e4af8a23a509f02d5d12eba94a4b936d40
Michel Melo da Silva <michel.silva66@gmail.com>
2024-12-16 12:12:50 -0300

    Adicionando ORCID.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 echo "Replacing urls..."
 
-sed -i 's/http:\/\/localhost:4000/./' *.html
-sed -i 's/<a href=".\/">Home<\/a>/<a href=".\/index.html">Home<\/a>/' *.html
-sed -i 's/\.\.\/assets/\.\/assets/' *.html
+sed -i 's/http:\/\/localhost:4000/./' _site/*.html
+sed -i 's/<a href=".\/">Home<\/a>/<a href=".\/index.html">Home<\/a>/' _site/*.html
+sed -i 's/\.\.\/assets/\.\/assets/' _site/*.html
 
 echo "Finish."
 
@@ -12,8 +12,8 @@ echo "Waiting 3s to complete changes."
 sleep 3s
 
 
-#echo "Starting deploy to http://www.dcc.ufmg.br/~michelms ..."
+echo "Starting deploy to http://www.dcc.ufmg.br/~michelms ..."
 
-#scp -r _site/* michelms@login.dcc.ufmg.br:~/public_html
+# scp -r _site/* michelms@login.dcc.ufmg.br:~/public_html
 
 echo "Finish."
